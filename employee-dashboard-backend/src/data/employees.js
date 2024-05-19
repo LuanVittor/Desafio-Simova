f7edfb109f2264c1621be649ce3aad3daeca502d
Luan Vitor Coelho de Araujo <luan_vittor@hotmail.com>
2024-05-18 21:51:22 -0300

    mocked data

---
+++ employee-dashboard-backend/src/data/employees.js
@@ -0,0 +1,11 @@
+module.exports = module.exports = [
+  { id: 1, name: 'Employee 1', code: '1', active: true, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee1.png' },
+  { id: 2, name: 'Employee 11', code: '3', active: true, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee2.png' },
+  { id: 3, name: 'Employee 17', code: '5', active: true, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee3.png' },
+  { id: 4, name: 'Employee 13', code: '7', active: true, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee4.png' },
+  { id: 5, name: 'Employee 21', code: '9', active: false, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee1.png' },
+  { id: 6, name: 'Employee 23', code: '11', active: false, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee2.png' },
+  { id: 7, name: 'Employee 25', code: '13', active: true, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee3.png' },
+  { id: 8, name: 'Employee 27', code: '15', active: false, image: 'https://raw.githubusercontent.com/daltondiaz/test-frontend/main/assets/images/employee4.png' },
+];
+  
